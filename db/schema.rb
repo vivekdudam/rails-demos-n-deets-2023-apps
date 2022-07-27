11c1ff42a08d6bc1515f54b260316eaab5dec8c0
Marshall Lussier <marshallelussier@gmail.com>
2022-07-27 15:55:46 -0500

    Create a Model Association Class

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_27_202308) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_27_205511) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_27_202308) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "project_assignments", force: :cascade do |t|
+    t.bigint "employee_id", null: false
+    t.bigint "project_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["employee_id"], name: "index_project_assignments_on_employee_id"
+    t.index ["project_id"], name: "index_project_assignments_on_project_id"
+  end
+
   create_table "projects", force: :cascade do |t|
     t.text "description"
     t.string "title"
@@ -30,4 +39,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_27_202308) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "project_assignments", "employees"
+  add_foreign_key "project_assignments", "projects"
 end
