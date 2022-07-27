bc0ed4dde3aaeca4e83d3864cfd06382deeb1fb1
Marshall Lussier <marshallelussier@gmail.com>
2022-07-27 14:25:07 -0500

    Add index, show, new, create, edit, update, and destroy resource routes and actions for Quiz model objects

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,15 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  root to: redirect('/quizzes')
+
+  get 'quizzes', to: 'quizzes#index', as: 'quizzes'
+  post 'quizzes', to: 'quizzes#create'
+  get 'quizzes/new', to: 'quizzes#new', as: 'new_quiz'
+  get 'quizzes/:id', to: 'quizzes#show', as: 'quiz'
+  patch 'quizzes/:id', to: 'quizzes#update'
+  delete 'quizzes/:id', to: 'quizzes#destroy'
+  get 'quizzes/:id/edit', to: 'quizzes#edit', as: 'edit_quiz'
+
 end
