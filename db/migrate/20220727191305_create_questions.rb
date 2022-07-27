29353479f7c165eecff2c259ab8333ac40c84a42
Marshall Lussier <marshallelussier@gmail.com>
2022-07-27 14:16:22 -0500

    Add Question model class with validations, seeds, fixtures, and model tests

---
+++ db/migrate/20220727191305_create_questions.rb
@@ -0,0 +1,12 @@
+class CreateQuestions < ActiveRecord::Migration[7.0]
+  def change
+    create_table :questions do |t|
+      t.string :answer
+      t.string :distractor_1
+      t.string :distractor_2
+      t.string :question
+
+      t.timestamps
+    end
+  end
+end
